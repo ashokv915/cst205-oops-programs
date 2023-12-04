070cd2e25b8697f2947926fb8991fb7367c7a7dc
ashokv915 <vashok915@gmail.com>
2023-12-04 15:33:13 +0530

    swing

---
+++ EventDemo.java
@@ -0,0 +1,86 @@
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EventDemo {
+
+    JLabel jlab;
+
+    EventDemo()
+    {
+        //Create a new JFrame container
+        JFrame jfrm = new JFrame("An event Example");
+
+        //Flowlayout
+        jfrm.setLayout(new FlowLayout());
+
+        jfrm.setSize(220,90);
+
+        //Terminate the program 
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton jbtnAlpha = new JButton("Alpha");
+        JButton jbtnBeta = new JButton("Beta");
+
+        JTextField jtf = new JTextField("enter", 7);
+        jtf.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                String txt = jtf.getText();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e1) {
+                    
+                    e1.printStackTrace();
+                }
+                jlab.setText(txt);
+            }
+        });
+
+
+        //Add action to Alpha button
+        jbtnAlpha.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                jlab.setText("Alpha was pressed");
+            }
+        });
+
+        //Add action to Beta button
+        jbtnBeta.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                jlab.setText("Beta was pressed");
+            }
+        });
+
+
+        jfrm.add(jbtnAlpha);
+        jfrm.add(jbtnBeta);
+        jfrm.add(jtf);
+
+        jlab = new JLabel("Press a button");
+
+        jfrm.add(jlab);
+
+        jfrm.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run()
+            {
+                new EventDemo();
+            }
+        });
+    }
+    
+}
---
+++ SwingDemo.java
@@ -0,0 +1,54 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwingDemo
+{
+    SwingDemo()
+    {
+        //Jframe container
+        JFrame jfrm = new JFrame("Simple Swing Application");
+
+        //set size of the frame
+        jfrm.setSize(275,100);
+
+        //Terminate the program when the user closes the application
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create a text based label
+        JLabel jlab = new JLabel("Swing means powerful UI");
+
+
+        JButton jbtnAlpha = new JButton("Alpha");
+        jbtnAlpha.setSize(100,50);
+        jbtnAlpha.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jlab.setText("Alpha pressed");
+            }
+        });
+
+        //Add the lable to the content pane
+        jfrm.add(jlab);
+
+        jfrm.add(jbtnAlpha);
+
+        jfrm.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        //create the frame on the event dispatching thread
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run()
+            {
+                new SwingDemo();
+            }
+        });
+    }
+}
